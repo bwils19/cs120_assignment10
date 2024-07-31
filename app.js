5952098a3c2bdc63475165c394360c064c81685e
Brandon Wilson <brandonlwilson19@gmail.com>
2024-07-30 22:51:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
